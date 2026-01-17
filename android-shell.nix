8bb211adadf29e7547e5eca956fdc27277fd30e1
c111spike <c111spike@gmail.com>
2026-01-17 07:29:12 +0000

    180 flip for coordinate sniper

---
+++ android-shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  androidSdk = pkgs.androidenv.composeAndroidPackages {
+    platformVersions = [ "34" ];
+    buildToolsVersions = [ "34.0.0" ];
+    includeNDK = false;
+    includeEmulator = false;
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.jdk17
+    androidSdk.androidsdk
+  ];
+
+  ANDROID_HOME = "${androidSdk.androidsdk}/libexec/android-sdk";
+  ANDROID_SDK_ROOT = "${androidSdk.androidsdk}/libexec/android-sdk";
+  JAVA_HOME = "${pkgs.jdk17}";
+
+  shellHook = ''
+    export PATH="${androidSdk.androidsdk}/libexec/android-sdk/platform-tools:$PATH"
+    export PATH="${androidSdk.androidsdk}/libexec/android-sdk/build-tools/34.0.0:$PATH"
+    echo "Android SDK and JDK 17 ready"
+  '';
+}
